9dd0a67c59c2a7f7d21dfb81a8c3020afb29c921
Camila Dziubat <goncalvesscamilla07@gmail.com>
2025-11-28 22:50:07 -0300

    Atualiza classificador.js e README.md

---
+++ classificador.js
@@ -0,0 +1,35 @@
+// Classificador de Nível de Herói
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+readline.question("Digite o nome do herói: ", nome => {
+    readline.question("Digite a quantidade de XP do herói: ", xpInput => {
+        let xp = parseInt(xpInput);
+        let nivel;
+
+        // Estrutura de decisão
+        if (xp < 1001) {
+            nivel = "Ferro";
+        } else if (xp <= 2000) {
+            nivel = "Bronze";
+        } else if (xp <= 5000) {
+            nivel = "Prata";
+        } else if (xp <= 7000) {
+            nivel = "Ouro";
+        } else if (xp <= 8000) {
+            nivel = "Platina";
+        } else if (xp <= 9000) {
+            nivel = "Ascendente";
+        } else if (xp <= 10000) {
+            nivel = "Imortal";
+        } else {
+            nivel = "Radiante";
+        }
+
+        console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
+        readline.close();
+    });
+});
